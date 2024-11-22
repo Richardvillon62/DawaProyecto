6cf2d5fcd83a156a17cd6455bfe51c27746c6045
Richardvillon62 <richardpablovillon.99@gmail.com>
2024-11-22 00:04:53 -0500

    Update App.js

--- dawa_proyecto/src/App.js
+++ dawa_proyecto/src/App.js
@@ -1,6 +1,7 @@
-
 import './App.css';
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
