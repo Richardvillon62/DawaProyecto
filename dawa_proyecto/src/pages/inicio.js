9fab26c5699570828cf6015f40fa3a83f2ec3079
Richardvillon62 <richardpablovillon.99@gmail.com>
2024-11-22 00:34:49 -0500

    codigo

--- dawa_proyecto/src/pages/inicio.js
+++ dawa_proyecto/src/pages/inicio.js
@@ -1,2 +1,15 @@
 import React from "react";
 import Navegacion from '../components/navbar.jsx';
+import FooterPage from '../components/footer.jsx';
+import Carousel from "../components/carrousel.jsx";
+
+export default function () {
+
+    return (
+        <div>
+            <Navegacion></Navegacion>
+            <Carousel></Carousel>
+            <FooterPage></FooterPage>
+        </div>
+    );
+}
