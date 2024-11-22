68e0a4f03e663f8851ebc962debfa531c4e537db
Richardvillon62 <richardpablovillon.99@gmail.com>
2024-11-22 00:16:40 -0500

    Update navbar.jsx

--- dawa_proyecto/src/components/navbar.jsx
+++ dawa_proyecto/src/components/navbar.jsx
@@ -4,6 +4,8 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import { getByTitle } from '@testing-library/react';
 import { Divider } from '@mui/material';
+import MobileFriendlyIcon from '@mui/icons-material/MobileFriendly';
+import { NavLink, useNavigate } from 'react-router-dom';
 
 
 export default function Navbar() {
