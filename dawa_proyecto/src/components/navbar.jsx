2726af45151fce3106c4cb3409083946d2882b59
Richardvillon62 <richardpablovillon.99@gmail.com>
2024-11-22 00:16:22 -0500

    Update navbar.jsx

--- dawa_proyecto/src/components/navbar.jsx
+++ dawa_proyecto/src/components/navbar.jsx
@@ -3,11 +3,12 @@ import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import { getByTitle } from '@testing-library/react';
+import { Divider } from '@mui/material';
 
 
 export default function Navbar() {
     const pages = [
         { title: 'Inicio', path: '/'},
-        
+
     ];
 }
