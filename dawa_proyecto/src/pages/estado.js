f0a5bd1ce8aeeaf6aa87f923561914847c6052a1
Richardvillon62 <richardpablovillon.99@gmail.com>
2024-11-25 17:57:07 -0500

    Create estado.js

---
+++ dawa_proyecto/src/pages/estado.js
@@ -0,0 +1,22 @@
+import React from'react';
+
+import Navegacion from '../components/navbar.jsx';
+import EstadoAdministrador from '../components/from_estadoAdmin.jsx';
+import EstadoCliente from '../components/from_estadoCliente.jsx';
+import Footer from '../components/footer.jsx';
+
+function Estados() {
+
+
+  return (
+    <div>
+      <Navegacion></Navegacion>
+      <EstadoAdministrador></EstadoAdministrador>
+      <EstadoCliente></EstadoCliente>
+      <Footer></Footer>
+    </div>
+
+)}
+
+export default Estados;
+
